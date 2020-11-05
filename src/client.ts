3208464ec42ac2e4700937065c2d074d95b0e391
Mahdi Yari <m.yary16@gmail.com>
2020-11-06 01:04:34 +0330

    extra check

--- src/client.ts
+++ src/client.ts
@@ -307,7 +307,7 @@ export class Client {
         }
         const body = JSON.stringify(request, (key, value) => {
             // encode Buffers as hex strings instead of an array of bytes
-            if (typeof value === 'object' && value.type === 'Buffer') {
+            if (value && typeof value === 'object' && value.type === 'Buffer') {
                 return Buffer.from(value.data).toString('hex')
             }
             return value
